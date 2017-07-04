2c348c99d91fd9f01c2c465154f8c2dfad78a1b7
Neurosoft\n.lyrakis <n.lyrakis@neurosoft.gr>
2017-07-04 14:42:04 +0300

    fixed css imported bootstrap to app from agular cli json

--- pharma-shop-app/src/app/app.component.ts
+++ pharma-shop-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css','../css/bootstrap.min.css','../css/shop-homepage.css']
+  styleUrls: ['./app.component.css','../css/shop-homepage.css']
 })
 export class AppComponent {
   title = 'app';
